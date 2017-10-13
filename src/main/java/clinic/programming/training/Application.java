cff60a91bc2be1c0b26e47df4968dad0056f2a8c
unknown <unknown@8216ee34-c643-91f1-7ee8-ce57b7db7bff>
2017-10-13 13:14:29 +0000

    apache commons added
    git-svn-id: https://github.com/gchadive/maven-quick-start.git/trunk@5 8216ee34-c643-91f1-7ee8-ce57b7db7bff

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,36 @@
 package clinic.programming.training;
+import java.util.List;
+import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
+	
+	public int countWords(String words) {
+	String[] seperateWords = StringUtils.split(words,' ');
+	return (seperateWords==null)?0:seperateWords.length;
+	
+	}
+	
+	public void greet() {
+		List<String> greetings = new ArrayList<> ();
+		greetings.add("Hello");
+		
+		for (String greeting : greetings) {
+			System.out.println("test "+greeting);
+		}
+	}
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	app.greet();
+	int count = app.countWords("I hava four words");
+	System.out.println("no of words :"+count);
     }
+	
 }
